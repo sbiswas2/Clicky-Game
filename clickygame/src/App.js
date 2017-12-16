982d864172d382223fab599ef301fb0d98e01f1d
Setu Biswas <setubiswas@Setus-MacBook-Pro.local>
2017-12-15 19:56:42 -0600

    first edit

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './public/assets/images/basketball.jpg';
+import './public/assets/css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Clicky Game!</h1>
+          <h3>Click on a Team Logo to earn points, but don't click on any more than once!</h3>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
